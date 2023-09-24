897b75ddfb6d5ebf4a3828b856952a068f6dffd8
Arthur Carvalho da Silva <arthursilvainformatica@gmail.com>
2023-09-24 00:16:33 -0300

    Solução 1

--- div_figurinhas.py
+++ div_figurinhas.py
@@ -4,19 +4,30 @@ N = 0
 F1 = int(input("Qtd de Figurinhas? "))
 F2 = int(input("Qtd de Figurinhas? "))
 cont = 0
+qtd = 0
+Result_F1 = 0
+Result_F2 = 0
 
-while(cont == 0):
-    N = N + 1    
-    if F1 % N == 0:
-        Result_F1 = F1 / 2
-        cont  = 1
+if F1 > F2:
+    while(cont == 0):    
+        F1 = F1 - 1
+        if F1 == F2:
+            Result_F1 = F1
+            cont = 1
+            F1 = F1 + qtd
+        qtd = 1 + qtd
+           
 
 cont = 0
 
-while(cont == 0):
-    if F2 % N == 0:
-        Result_F2 = F1 / 2
-        cont  = 1  
+if F1 < F2:
+    while(cont == 0):    
+        F2 = F2 - 1
+        if F2 == F1:
+            Result_F2 = F2
+            cont = 1
+            F2 = F2 + qtd
+        qtd = 1 + qtd
   
 print(Result_F1)
 print(Result_F2)
